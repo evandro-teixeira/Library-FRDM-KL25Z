09ca1b30572fe3a7a7986b89601c666f4554c5b6
evandro-teixeira <evandrolst@live.com>
2017-04-17 12:37:07 -0300

    uart
    arquivos uart

---
+++ externs.h
@@ -0,0 +1,22 @@
+/*
+ * externs.h
+ *
+ *  Created on: Jul 20, 2016
+ *      Author: Evandro
+ */
+
+#ifndef EXTERNS_H_
+#define EXTERNS_H_
+
+#include <MKL25Z4.h>    /* I/O map for MKL25Z128VLK4 */
+#include "stdbool.h"
+#include "externs.h"
+#include "gpio.h"
+#include "uart.h"
+#include "adc.h"
+#include "spi.h"
+#include "i2c.h"
+
+//extern uint16_t iClock;
+
+#endif /* EXTERNS_H_ */
