3c456a3af5c06663377b5d456084c37feb27f48a
Evandro Teixeira <evandrolst@yahoo.com.br>
2017-04-21 15:34:21 -0300

    dac
    arquivos dac

---
+++ dac.h
@@ -0,0 +1,18 @@
+/*
+ * dac.h
+ *
+ *  Created on: 21/04/2017
+ *      Author: evandro
+ */
+
+#ifndef SOURCES_DAC_H_
+#define SOURCES_DAC_H_
+
+#include "externs.h
+//#include "MKL25Z4.h"
+//#include "stdbool.h"
+
+void dac_Init(void);
+bool dac_Output(uint16_t value);
+
+#endif /* SOURCES_DAC_H_ */
